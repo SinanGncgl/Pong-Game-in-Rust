969f9103b492a0716abdfcfa7e2012136473c748
Sinan Gencoglu <sinan.gencogluu@gmail.com>
2023-03-24 01:31:46 +0300

    draw a paddle

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+
 use ggez;
 use ggez::{
+    Context,
     GameResult, 
     GameError,
     graphics,
     event,
 };
+
+const PADDLE_HEIGHT:f32 = 85.;
+const PADDLE_WIDTH:f32 = 18.;
 
 struct MainState {}
 
@@ -15,19 +20,26 @@ impl MainState {
 }
 
 impl  event::EventHandler for MainState {
-    fn update(&mut self, _ctx: &mut ggez::Context) -> Result<(), GameError> {
+    fn update(&mut self, _ctx: &mut Context) -> Result<(), GameError> {
         Ok(())
     }
-    fn draw(&mut self, ctx: &mut ggez::Context) -> Result<(), GameError> {
-        let canvas = graphics::Canvas::from_frame(ctx, graphics::Color::BLACK);
-        _ = canvas.finish(ctx);
+    fn draw(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+        let mut canvas = graphics::Canvas::from_frame(ctx, graphics::Color::BLACK);
+
+        let paddle = graphics::Rect::new(30., 230., PADDLE_WIDTH, PADDLE_HEIGHT);
+        let paddle_mesh = graphics::Mesh::new_rectangle(ctx, 
+                graphics::DrawMode::fill(), paddle, graphics::Color::BLUE)?;
+
+        canvas.draw(&paddle_mesh, graphics::DrawParam::default());
+
+        canvas.finish(ctx)?;
         Ok(())
     }
 }
 
 fn main() -> GameResult {
-    let ctx = ggez::ContextBuilder::new("Pong", "Sinan Gencoglu");
-    let (ctx, event_loop) = ctx.build()?; // (?): build can fail
+    let cb = ggez::ContextBuilder::new("Pong", "Sinan Gencoglu");
+    let (ctx, event_loop) = cb.build()?; // (?): build can fail
     ctx.gfx.set_window_title("PONG");
 
     let state = MainState::new();
